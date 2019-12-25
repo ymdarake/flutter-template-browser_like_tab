4860feae546fbbaf087ed8459f6673afe5256b80
ymdarake <t.sakamoto.oeun@gmail.com>
2019-12-25 19:43:02 +1300

    [update]Columnize

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -7,43 +7,23 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   Animation<double> tabAnimation;
-  Animation<double> adAnimation;
-  Animation<double> bottomSheetAnimation;
   AnimationController tabAnimationController;
-  final double bottomSheetHeight = 500.0; // TODO: preferable: calc dynamically
+  final double toolbarHeight = 20.0; // TODO: calc.
+  final double topMargin = 20.0;
   final double adHeight = 60.0;
+  double bottomSheetHeight;
   final double appBarHeight = 32.0;
+  final double bottomNavigationBarHeight = 60.0;
 
   @override
   void initState() {
     super.initState();
-    tabAnimationController = AnimationController(
-      duration: Duration(milliseconds: 300),
-      vsync: this,
-    );
-    tabAnimation = Tween(begin: 0.0, end: -bottomSheetHeight).animate(
-      CurvedAnimation(
-        curve: Curves.easeInOut,
-        parent: tabAnimationController,
-      ),
-    );
-    adAnimation = Tween(begin: 0.0, end: bottomSheetHeight).animate(
-      CurvedAnimation(
-        curve: Curves.easeInOut,
-        parent: tabAnimationController,
-      ),
-    );
-    bottomSheetAnimation =
-        Tween(begin: -adHeight - bottomSheetHeight, end: 0.0).animate(
-      CurvedAnimation(
-        curve: Curves.easeInOut,
-        parent: tabAnimationController,
-      ),
-    );
   }
 
   @override
   Widget build(BuildContext context) {
+    _initializeAnimation();
+
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(appBarHeight),
@@ -56,6 +36,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
           ),
         ),
       ),
+      bottomNavigationBar: _buildBottomNavigationBar(),
       backgroundColor: Theme.of(context).colorScheme.primary,
       body: _buildTab(),
     );
@@ -63,106 +44,113 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
 
   Widget _buildTab() {
     return GestureDetector(
-      onVerticalDragDown: null,
-      onVerticalDragUpdate: _handleDragUpdate,
-      child: Stack(
-        overflow: Overflow.visible,
-        children: <Widget>[
-          _buildTabAnimation(),
-          _buildAdAnimation(),
-          _buildBottomSheetAnimation(),
-        ],
-      ),
-    );
+        onVerticalDragDown: null,
+        onVerticalDragUpdate: _handleDragUpdate,
+        child: Stack(
+          children: <Widget>[
+            _buildTabAnimation(),
+          ],
+        ));
   }
 
   Widget _buildTabAnimation() {
     return AnimatedBuilder(
       animation: tabAnimation,
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        height:
-            MediaQuery.of(context).size.height - 30 - appBarHeight - adHeight,
-        margin: EdgeInsets.only(top: 30),
-        decoration: BoxDecoration(
-          boxShadow: _buildTabShadow(),
-          color: Theme.of(context).colorScheme.surface,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(27.0),
-            topRight: Radius.circular(27.0),
-          ),
-        ),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Text('hoi!'),
-              Text('hoi!'),
-              Text('hoi!'),
-            ],
-          ),
-        ),
-      ),
       builder: (context, child) {
         return Positioned(
           top: tabAnimation.value,
-          child: child,
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height -
+                    toolbarHeight -
+                    topMargin -
+                    adHeight -
+                    appBarHeight -
+                    bottomNavigationBarHeight,
+                margin: EdgeInsets.only(top: topMargin),
+                decoration: BoxDecoration(
+                  boxShadow: _buildTabShadow(),
+                  color: Theme.of(context).colorScheme.surface,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(27.0),
+                    topRight: Radius.circular(27.0),
+                  ),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Text('Hi!'),
+                    Text('Hi!'),
+                    Text('Hi!'),
+                  ],
+                ),
+              ),
+              _buildAd(),
+              _buildBottomSheet(),
+            ],
+          ),
         );
       },
     );
   }
 
-  Widget _buildAdAnimation() {
-    return AnimatedBuilder(
-      animation: adAnimation,
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        height: adHeight,
-        decoration: BoxDecoration(
-          color: Colors.green,
-        ),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Text('Ad!'),
-            ],
-          ),
+  Widget _buildAd() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: adHeight,
+      decoration: BoxDecoration(
+        color: Colors.green,
+      ),
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Text('Ad!'),
+          ],
         ),
       ),
-      builder: (context, child) {
-        return Positioned(
-          bottom: adAnimation.value,
-          child: child,
-        );
-      },
     );
   }
 
-  Widget _buildBottomSheetAnimation() {
-    return AnimatedBuilder(
-      animation: bottomSheetAnimation,
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        height: bottomSheetHeight,
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.error,
-        ),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Text('bottom'),
-              Text('bottom'),
-              Text('bottom'),
-              Text('bottom'),
-            ],
-          ),
+  Widget _buildBottomSheet() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: bottomSheetHeight,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.error,
+      ),
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Text('bottom'),
+            Text('bottom'),
+            Text('bottom'),
+            Text('bottom'),
+          ],
         ),
       ),
-      builder: (context, child) {
-        return Positioned(
-          bottom: bottomSheetAnimation.value,
-          child: child,
-        );
-      },
+    );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return SizedBox(
+      height: bottomNavigationBarHeight,
+      child: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            title: Text('Calendar'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            title: Text('Caht'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Settings'),
+          ),
+        ],
+      ),
     );
   }
 
@@ -186,5 +174,28 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
         ),
       ),
     ];
+  }
+
+  _initializeAnimation() {
+    if (tabAnimation != null) return;
+
+    bottomSheetHeight = MediaQuery.of(context).size.height;
+    tabAnimationController = AnimationController(
+      duration: Duration(milliseconds: 300),
+      vsync: this,
+    );
+    tabAnimation = Tween(
+            begin: 0.0,
+            end: -bottomSheetHeight +
+                adHeight +
+                appBarHeight +
+                toolbarHeight +
+                topMargin)
+        .animate(
+      CurvedAnimation(
+        curve: Curves.easeInOut,
+        parent: tabAnimationController,
+      ),
+    );
   }
 }
